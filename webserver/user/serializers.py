280a85a5693b17faeaa24ce838afb3e7bf5efa04
Jhonatan <jhonatandaar@gmail.com>
2021-09-25 13:22:39 -0500

    Finished serializers for all models

---
+++ webserver/user/serializers.py
@@ -0,0 +1,79 @@
+from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
+from .models import User, Semester, Grade, Parent
+
+
+class UserSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            password=make_password(
+                validated_data['user'].pop('password')
+            ),
+            **validated_data.pop('user')
+        )
+
+    def update(self, instance, validated_data):
+        if 'user' in validated_data:
+            instance.user.password = make_password(
+                validated_data.get('user').get(
+                    'password', instance.user.password)
+            )
+            instance.user.save()
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'password',
+            'fullname',
+            'document',
+            'expeditionOfDocumentDepartment',
+            'expeditionOfDocumentPlace',
+            'sex',
+            'etnicity',
+            'personalEmail',
+            'institutionalEmail',
+            'cellphoneNumber',
+            'phoneNumber',
+            'profileImage',
+            'birthDate',
+            'birthPlace',
+            'nationality',
+            'bloodType',
+            'rhFactor',
+            'eps',
+            'direction',
+            'stratum',
+            'militarService'
+        )
+
+
+class SemesterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Semester
+        fields = (
+            'user',
+            'semesterName'
+        )
+
+
+class GradeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Grade
+        fields = (
+            'semester',
+            'courseName',
+            'courseFinalGrade',
+            'courseGradeText'
+        )
+
+
+class ParentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Parent
+        fields = (
+            'user',
+            'name',
+            'typeOfDocument',
+            'document'
+        )
