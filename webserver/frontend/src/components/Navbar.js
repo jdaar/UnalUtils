8624cfbd33f5a9174f7fcbfa288ee92f6cae5a75
Jhonatan <jhonatandaar@gmail.com>
2021-09-25 12:26:39 -0500

    Finished setting up styled-components

--- webserver/frontend/src/components/Navbar.js
+++ webserver/frontend/src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { Button } from "./Styled"
 
 const Navbar = ({links}) => {
 	return (
 		<div className="navbar">
 		{links.map(link => {
-			return <Link to={link.route}><h3>{link.name}</h3></Link>
+			return <Link to={link.route}><Button>{link.name}</Button></Link>
 		})}
 		</div>
 	)
---
+++ webserver/frontend/src/components/Styled.js
@@ -0,0 +1,8 @@
+import styled, { css } from 'styled-components'
+
+export const Button = styled.button`
+	background-color: aquamarine;
+	padding: 1em;
+	margin: 1em;
+	color: white;
+`
